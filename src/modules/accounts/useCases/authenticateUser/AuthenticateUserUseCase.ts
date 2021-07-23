d6e79719a0eb9a3d799c045f64b6ce8a6da495a9
thiago <thiagosteps@gmail.com>
2021-07-23 14:19:24 +0100

    refresh token implemented

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -3,6 +3,9 @@ import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { sign } from 'jsonwebtoken';
 import { AppError } from "@shared/errors/AppError";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import auth from "@config/auth";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 
 
 interface IRequest {
@@ -16,17 +19,25 @@ interface IResponse {
         email: string
     },
     token: string;
+    refresh_token: string;
 }
 
 @injectable()
 class AuthenticateUserUseCase {
     constructor(
         @inject('UsersRepository')
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        @inject('UsersTokensRepository')
+        private usersTokensRepository: IUsersTokensRepository,
+        @inject("DayjsDateProvider")
+        private dateProvider: IDateProvider
     ) {}
 
     async execute({ email, password }: IRequest): Promise<IResponse> {
         const user = await this.usersRepository.findByEmail(email);
+        const { expires_in_token, secret_refresh_token, secret_token,
+            expires_in_refresh_token,
+            expires_refresh_token_days } = auth;
 
         if (!user) {
             throw new AppError("Email or password incorrect!");
@@ -38,17 +49,33 @@ class AuthenticateUserUseCase {
             throw new AppError("Email or password incorrect!")
         }
 
-        const token = sign({}, 'cfe275a5908b5650488e0342c2d6cc', {
+        const token = sign({}, secret_token, {
             subject: user.id,
-            expiresIn: '1d',
+            expiresIn: expires_in_token,
         });
+
+        const refresh_token = sign({ email }, secret_refresh_token, {
+            subject: user.id,
+            expiresIn: expires_in_refresh_token
+        });
+
+        const refresh_token_expires_date = this.dateProvider.addDays(
+            expires_refresh_token_days
+        )
+
+        await this.usersTokensRepository.create({
+            expires_date: refresh_token_expires_date,
+            user_id: user.id,
+            refresh_token,
+        })
 
         const tokenReturn: IResponse = {
             token, 
             user: {
                 name: user.name,
                 email: user.email
-            }
+            },
+            refresh_token
         }
 
         return tokenReturn;
--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,8 +6,7 @@ import utc from 'dayjs/plugin/utc'
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
- 
-  
+   
   compareInHours(start_date: Date, end_date: Date): number {
     const end_date_utc = this.convertToUTC(end_date);
     const start_date_utc = this.convertToUTC(start_date);
@@ -31,6 +30,10 @@ class DayjsDateProvider implements IDateProvider {
     const start_date_utc = this.convertToUTC(start_date);
 
     return dayjs(end_date_utc).diff(start_date_utc, "days");
+  }
+
+  addDays(days: number): Date {
+    return dayjs().add(days, "days").toDate();
   }
 
 }
