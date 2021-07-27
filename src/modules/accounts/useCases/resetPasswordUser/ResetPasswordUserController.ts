f75c5a168dc1aef1d60f60f386ac5772cadf1e6f
thiago <thiagosteps@gmail.com>
2021-07-27 10:16:50 +0100

    resetPassword implemented

---
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController.ts
@@ -0,0 +1,22 @@
+import { container } from "tsyringe";
+import { ResetPasswordUserUseCase } from "./ResetPasswordUserUseCase";
+import { Request, Response } from "express";
+
+
+
+
+class ResetPasswordUserController {
+
+  async handle(request: Request, response: Response): Promise<Response> {
+
+    const { token } = request.query;
+    const { password } =  request.body;
+
+    const resetPasswordUserUseCase = container.resolve(ResetPasswordUserUseCase);
+    await resetPasswordUserUseCase.execute({ token: String(token), password});
+
+    return response.send();
+  }
+}
+
+export { ResetPasswordUserController };
--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,3 +1,4 @@
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 import { Router } from "express";
 
@@ -6,7 +7,9 @@ import { Router } from "express";
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordUserController.handle);
 
 export { passwordRoutes };
