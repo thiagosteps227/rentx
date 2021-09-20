679bc08169117be8a9ebf378be6dd23de564dcba
thiago <thiagosteps@gmail.com>
2021-09-20 16:56:54 +0100

    uploading images with no visual on localhost bug

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,10 +1,11 @@
-import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
-import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
-import { Router } from 'express';
-import multer from 'multer';
+import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { Router } from "express";
+import multer from "multer";
 
-import uploadConfig  from '@config/upload';
-import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import uploadConfig from "@config/upload";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 
 const usersRoutes = Router();
 
@@ -12,12 +13,17 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
-usersRoutes.post('/', createUserController.handle);
+usersRoutes.post("/", createUserController.handle);
 
-usersRoutes.patch('/avatar',
-    ensureAuthenticated,
-    uploadAvatar.single('avatar'),
-    updateUserAvatarController.handle)
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
+
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
