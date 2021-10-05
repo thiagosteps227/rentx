09edae603b797984cda6982336f9715b61c5dc18
thiago <thiagosteps@gmail.com>
2021-10-05 10:56:07 +0100

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -12,6 +12,7 @@ import createConnection from '@shared/infra/typeorm';
 import '@shared/container';
 import { AppError } from '@shared/errors/AppError';
 import upload from '@config/upload';
+import cors from "cors";
 
 createConnection();
 const app = express();
@@ -23,6 +24,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, request:Request, response:Response, next:NextFunction) => {
