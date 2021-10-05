b038f0d6119c7c5d6740d6ba9e7cc7c4a75dbeaf
thiago <thiagosteps@gmail.com>
2021-10-05 12:13:13 +0100

    sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,29 +1,44 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import "dotenv/config";
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 
-import swaggerUi from 'swagger-ui-express';
-import { router } from './routes';
+import swaggerUi from "swagger-ui-express";
+import { router } from "./routes";
 
-import swaggerFile from '../../../swagger.json';
-import createConnection from '@shared/infra/typeorm';
+import swaggerFile from "../../../swagger.json";
+import createConnection from "@shared/infra/typeorm";
 
-import '@shared/container';
-import { AppError } from '@shared/errors/AppError';
-import upload from '@config/upload';
+import "@shared/container";
+import { AppError } from "@shared/errors/AppError";
+import upload from "@config/upload";
 import cors from "cors";
 
-import rateLimiter from "@shared/infra/http/middlewares/rateLimiter"
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
+
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 createConnection();
 const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });//tem que ser depois do rateLimiter
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
@@ -31,17 +46,21 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 app.use(router);
 
-app.use((err: Error, request:Request, response:Response, next:NextFunction) => {
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
-        return response.status(err.statusCode).json({
-            message: err.message
-        });
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
     }
 
     return response.status(500).json({
-        status: 'error',
-        message: `Internal server error - ${err.message}`,
+      status: "error",
+      message: `Internal server error - ${err.message}`,
     });
-});
+  }
+);
 
 export { app };
